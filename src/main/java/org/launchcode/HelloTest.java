517e3be0aa0754e3b9b6477f1cb364ba48cdd8b8
RCI\Alpi.Jain <alpi.jain@rci.rogers.ca>
2021-11-09 01:11:57 -0500

    added new sum function

---
+++ src/main/java/org/launchcode/HelloTest.java
@@ -0,0 +1,9 @@
+package org.launchcode;
+
+public class HelloTest {
+
+    public static void main()
+    {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/java/org/launchcode/Sampleclass.java
@@ -0,0 +1,16 @@
+package org.launchcode;
+
+public class Sampleclass {
+
+    public void sum()
+    {
+        int a=1;
+        int b=a+1;
+        int sum;
+        sum=a+b;
+        System.out.println(sum);
+
+
+
+    }
+}
