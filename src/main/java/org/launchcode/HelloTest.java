fbf7a6528e0c96d5c50b0e39342fef16452f32c8
RCI\Alpi.Jain <alpi.jain@rci.rogers.ca>
2021-11-09 02:07:29 -0500

    Added product

--- src/main/java/org/launchcode/HelloTest.java
+++ src/main/java/org/launchcode/HelloTest.java
@@ -5,5 +5,12 @@ public class HelloTest {
     public static void main()
     {
         System.out.println("Hello world");//gghello
+
+    }
+
+    public static void productdesc()
+    {
+        System.out.println("Product Desc");
+
     }
 }
